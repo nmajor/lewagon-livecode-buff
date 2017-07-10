8a9f76c286e662175292a64c8d608b4ab95a8904
Nicholas Major <nick@nmajor.com>
2017-07-10 17:03:13 +0100

    Randomly generating merit score and tweaking merit show page

--- app/models/merit.rb
+++ app/models/merit.rb
@@ -1,8 +1,14 @@
 class Merit < ApplicationRecord
   mount_uploader :image, ImageUploader
+  before_save :set_points, :if => Proc.new { |model| !model.points }
 
   def self.random
     offset = rand(self.count)
     self.offset(offset).first
+  end
+
+  private
+  def set_points
+    self.points = rand(1...100)
   end
 end
